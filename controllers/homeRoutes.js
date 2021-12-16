da51794abd3ae9b959ea0a9337d9fc7cc4b74afd
aod1411 <nothingbutgood@comcast.net>
2021-12-16 11:30:31 -0500

    techBlog

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
-//const { User } = require('../models')
+const { User, Post } = require('../models')
 
+router.get("/", async (req, res)=>{
+    try{
+        const postData = await Post.findAll({
+            include: [User]
+        });
 
+        const posts = postData.map((post) => post.get({ plain: true}))
+
+        res.render("all-posts", { posts })
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
 
 router.get("/login", (req, res)=>{
     if(req.session.loggedIn){
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path")
 const express = require("express")
 const session = require("express-session")
 const exphbs = require("express-handlebars")
+const routes = require("./controllers")
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -29,7 +30,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.use(require("./controllers"))
+app.use(routes)
 
 app.listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}`)
